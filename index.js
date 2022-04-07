1e667bd5c1bd28b1e35640fa2244ccca865e8ff2
brinkbrink <brinkbrink@gmail.com>
2022-04-07 10:24:09 -0700

    index.js v2

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ http.createServer((req, res) => {
   switch(path) {
     case '/':
       res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('Home page');
+      res.end('What is a home? Whatever it is, you\'re here.');
       break;
     case '/about':
       res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('About page');
+      res.end('Learning more about me could lead to learning more about you. Or it could just be a complete waste of time.');
       break;
     default:
       res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('404 error - Content not found');
+      res.end('404 error - These aren\'t the droids you\'re looking for...');
       break;
   }
 }).listen(process.env.PORT || 3000);
