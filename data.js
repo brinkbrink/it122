4c95d03888e5f61a8145bad27622ad71b5665cd1
brinkbrink <brinkbrink@gmail.com>
2022-04-16 09:32:00 -0700

    tapes data

---
+++ data.js
@@ -0,0 +1,55 @@
+const tapes = [
+    {
+        artist: 'Armand Hammer',
+        title: 'Shrines',
+        year: 2020,
+        genre: 'hip hop',
+        price: 13.00
+    },
+    {
+        artist: 'Death',
+        title: 'Leprosy',
+        year: 1988,
+        genre: 'death metal',
+        price: 11.00
+    },
+    {
+        artist: 'Matt Sweeney & Bonnie \'Prince\' Billy',
+        title: 'Superwolves',
+        year: 2021,
+        genre: 'folk rock',
+        price: 19.95
+    },
+    {
+        artist: 'Pilgrim Screw',
+        title: 'Pilgrim Screw',
+        year: 2021,
+        genre: 'lo-fi synth-punk',
+        price: 6.95
+    },
+    {
+        artist: 'Pink Siifu & Fly Anakin',
+        title: 'Fly Siifu',
+        year: 2020,
+        genre: 'hip hop',
+        price: 11.99
+    },
+    {
+        artist: 'Spirito Di Lupo',
+        title: '4 canzoni',
+        year: 2021,
+        genre: 'inner peace punk',
+        price: 9.07
+    },   
+    {
+        artist: 'Todd Rundgren',
+        title: 'The Ever Popular Tortured Artist Effect',
+        year: 1982,
+        genre: 'pop rock',
+        price: 3.79
+    }
+]
+
+const getAll = () => {
+    return tapes;
+}
