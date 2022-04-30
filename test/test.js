d79588d93b6072189897a7a6c62abf36d4947d7d
E. Brink <brinkbrink@gmail.com>
2022-04-30 14:24:21 -0700

    getItem test

--- test/test.js
+++ test/test.js
@@ -3,12 +3,12 @@ import * as tape from "../data.js";
 
 describe("tapes data module", () => {
  it("returns requested tape", function() {
-   var result = tape.getItem("Death");
+   let result = tape.getItem("Death");
    expect(result).to.deep.equal({artist: 'Death', title: 'Leprosy', year: 1988, genre: 'death metal', price: 11.00});
  });
 
- it("fails with invalid tape", () => {
-   var result = tape.getItem("fake");
+ it("fails with nonexistent tape", () => {
+   let result = tape.getItem("fake");
    expect(result).to.be.undefined;
  });
 });
