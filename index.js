550d8de08f2e623c4ab02a1870033273b8092c1a
E. Brink <brinkbrink@gmail.com>
2022-05-07 16:42:21 -0700

    ready for submit

--- index.js
+++ index.js
@@ -16,37 +16,21 @@ app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
   Tape.find({}).lean().then((tapes) => {
-      // respond to browser only after db query completes
       res.render('home', { tapes });
   })
 })
 
 app.get('/detail', (req,res) => {
-  // db query can use request parameters
   Tape.findOne({ artist:req.query.artist }).lean().then((tape) => {
           res.render('details', {result: tape} )
       })
 })
 
-// send static file as response
-// app.get('/', (req,res) => {
-//   res.type('text/html');
-//   res.render('home', { tapes: tape.getAll()});
-// });
-
-// app.get('/detail', (req,res) => {
-//   res.type('text/html');
-//   let result = tape.getItem(req.query.artist);
-//   res.render("details", { artist: req.query.artist, result })
-// });
-
-// send plain text response
 app.get('/about', (req,res) => {
   res.type('text/plain');
   res.send('About page');
 });
  
-// define 404 handler
 app.use((req,res) => {
   res.type('text/plain');
   res.status(404);
