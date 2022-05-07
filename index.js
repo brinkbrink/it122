7f6b6766adb7a68f4b6c904dd7b0a540561c82f8
E. Brink <brinkbrink@gmail.com>
2022-05-07 16:36:02 -0700

    home route figured out

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-// import http from 'http';
-import * as tape from './data.js';
+// // import http from 'http';
+// import * as tape from './data.js';
 // import path from 'path';
 import express from 'express';
+import { Tape } from "./Tapes.js";
 
 
 const app = express();
@@ -12,17 +13,32 @@ app.use(express.urlencoded({ extended: true }));//Parse URL-encoded bodies
 app.use(express.json()); //Used to parse JSON bodies
 app.set("view engine", "ejs");
 
-// send static file as response
-app.get('/', (req,res) => {
-  res.type('text/html');
-  res.render('home', { tapes: tape.getAll()});
-});
+
+app.get('/', (req, res) => {
+  Tape.find({}).lean().then((tapes) => {
+      // respond to browser only after db query completes
+      res.render('home', { tapes });
+  })
+})
 
 app.get('/detail', (req,res) => {
-  res.type('text/html');
-  let result = tape.getItem(req.query.artist);
-  res.render("details", { artist: req.query.artist, result })
-});
+  // db query can use request parameters
+  Tape.findOne({ artist:req.query.artist }).lean().then((tape) => {
+          res.render('details', {result: tape} )
+      })
+})
+
+// send static file as response
+// app.get('/', (req,res) => {
+//   res.type('text/html');
+//   res.render('home', { tapes: tape.getAll()});
+// });
+
+// app.get('/detail', (req,res) => {
+//   res.type('text/html');
+//   let result = tape.getItem(req.query.artist);
+//   res.render("details", { artist: req.query.artist, result })
+// });
 
 // send plain text response
 app.get('/about', (req,res) => {
@@ -30,7 +46,7 @@ app.get('/about', (req,res) => {
   res.send('About page');
 });
  
- // define 404 handler
+// define 404 handler
 app.use((req,res) => {
   res.type('text/plain');
   res.status(404);
