2ad394f25e48393472b41ebae80bd7a88022d21e
E. Brink <brinkbrink@gmail.com>
2022-05-23 17:17:49 -0700

    fixed error

--- index.js
+++ index.js
@@ -61,62 +61,34 @@ app.get('/delete/:artist', (req,res) => {
   let artist = req.params.artist;
   Tape.deleteOne({ artist:artist}, (err, result) => {
     if (result.deletedCount === 0){
-        console.log(err, "this don't work", result);
         res.render('delete', {result})
     }else{
         res.type('text/html');
         res.render('delete', {result: Tape}) 
-        console.log("Result :", result, artist, " deleted");
     }
 });
 });
 
 app.get('/api/v1/delete/:title', (req,res, next) => {
   Tape.deleteOne({"title":req.params.title }, (err, result) => {
-      if (err) return next(err);
-      console.log(result.deletedCount, "deleted")
-      res.json({result});
+    if (err || !result) {
+      res.status(200).json({"message":"Database error, title not found"});
+  } else {
+      res.json( result );
+   }
   });
 });
 
 app.post('/api/v1/add', (req, res) => {
   const newTape = {'artist': req.body.artist, 'title': req.body.title, 'year': req.body.year, 'genre': req.body.genre, 'price': req.body.price }
   Tape.updateOne({'artist': req.body.artist,}, newTape, {upsert:true}, (err, result) => {
-      if (err) return next(err);
-      console.log(result);
-      res.json(newTape)
+    if (err || !result) {
+      res.status(200).json({"message":"Database error, tape not added"});
+  } else {
+      res.json( newTape );
+   }
   });
 });
-
-// app.post('/api/v1/add/', (req,res, next) => {
-//   console.log(req.body)
-//   if (!req.body._id) { 
-
-//       let tape = new Tape({artist:req.body.artist, title:req.body.title, year:req.body.year, genre:req.body.genre, price:req.body.price});
-//       tape.save((err,tape) => {
-//           if (err) return next(err);
-//           console.log(newTape)
-//           res.json({tape});
-//       });
-//   } else { 
-//       Tape.updateOne({artist:req.body.artist, title:req.body.title, year:req.body.year, genre:req.body.genre, price:req.body.price}, (err, result) => {
-//           if (err) return next(err);
-//           res.json({updated: result.nModified, _id: req.body._id});
-//       });
-//   }
-// });
-
-// app.post('/api/v1/add', (req, res) => {
-//   const data = new Tape({artist: req.body.artist, title: req.body.title, year: req.body.year, genre: req.body.genre, price: req.body.price});
-//   try{
-//     const saveData = data.save();
-//     res.status(200).json(saveData)
-//   } 
-//   catch(error) {
-//     res.status(400).json({message: error.message})
-//   }
-// })
-
  
 app.use((req,res) => {
   res.type('text/plain');
